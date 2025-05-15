3793d4784f41706f90cdc3cfa8011284b9e6e84f
YashpalGawali <gawaliyashpal@gmail.com>
2025-05-15 16:43:49 +0530

    @Retry used for calling the company service from department service

--- department-service/src/main/java/com/example/demo/department_service/controller/DepartmentController.java
+++ department-service/src/main/java/com/example/demo/department_service/controller/DepartmentController.java
@@ -1,6 +1,8 @@
 package com.example.demo.department_service.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.example.demo.department_service.dto.Company;
 import com.example.demo.department_service.dto.DepartmentDto;
@@ -22,18 +25,18 @@ import com.example.demo.department_service.entity.Department;
 import com.example.demo.department_service.proxy.CompanyServiceProxy;
 import com.example.demo.department_service.service.IDepartmentService;
 
-import lombok.AllArgsConstructor;
+import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
-@RequestMapping("department")  
+@RequestMapping("department")
 public class DepartmentController {
 
 	private final IDepartmentService deptserv;
-	
+
 	private final CompanyServiceProxy companyProxy;
-	
+
 	private Logger logger = LoggerFactory.getLogger(getClass());
-	
+
 	/**
 	 * @param deptserv
 	 * @param companyProxy
@@ -45,65 +48,98 @@ public class DepartmentController {
 	}
 
 	@PostMapping("/")
-	public ResponseEntity<ResponseDto> saveDepartment(@RequestBody Department department)
-	{	
+	public ResponseEntity<ResponseDto> saveDepartment(@RequestBody Department department) {
 		Department dept = deptserv.saveDepartment(department);
-		return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseDto(HttpStatus.CREATED.toString(), "Department "+dept.getDept_name()+" is saved successfully"));
-	} 
-	
+		if (dept != null) {
+			return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseDto(HttpStatus.CREATED.toString(),
+					"Department " + dept.getDept_name() + " is saved successfully"));
+		} else {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body(new ResponseDto(HttpStatus.INTERNAL_SERVER_ERROR.toString(),
+							"Department " + department.getDept_name() + " is not saved "));
+		}
+	}
+
 	@GetMapping("/")
+	@Retry(name = "getCompanyByIdForDeptList", fallbackMethod = "handleCompanyResponse")
 	public ResponseEntity<List<DepartmentDto>> getAllDepartments() {
 		List<Department> deptList = deptserv.getAllDepartments();
 
 		List<DepartmentDto> dept_list = deptList.stream().map(dept -> {
 
 			DepartmentDto deptdto = new DepartmentDto();
+
 			ResponseEntity<Company> company = companyProxy.getCompanyById(dept.getCompany_id());
 
-			companyProxy.logAndGetCompanyById(dept.getCompany_id());
-
-			Company companyObj = new Company();
-
-			companyObj = company.getBody();
-
+			Map<String, String> hashMap = new HashMap<>();
+			hashMap.put("id", dept.getCompany_id());
+ 
 			deptdto.setDept_id(dept.getDept_id());
 			deptdto.setDept_name(dept.getDept_name());
-			deptdto.setCompany(companyObj);
+			deptdto.setCompany(company.getBody());
 			return deptdto;
 
 		}).collect(Collectors.toList());
 		return ResponseEntity.status(HttpStatus.OK).body(dept_list);
 	}
-	
+
+	public ResponseEntity<List<DepartmentDto>> handleCompanyResponse(Exception ex) {
+
+		List<Department> allDepts = deptserv.getAllDepartments();
+		List<DepartmentDto> deptDtoList = allDepts.stream().map(d -> {
+			DepartmentDto deptdto = new DepartmentDto();
+
+			deptdto.setDept_id(d.getDept_id());
+			deptdto.setDept_name(d.getDept_name());
+
+			Company comp = new Company();
+			deptdto.setCompany(comp);
+
+			return deptdto;
+		}).collect(Collectors.toList());
+		return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(deptDtoList);
+	}
+
 	@GetMapping("/{id}")
+	@Retry(name = "getCompanyById", fallbackMethod = "handleGetCompanyByIdResponse")
 	public ResponseEntity<DepartmentDto> getDepartmentbyId(@PathVariable Integer id) {
-		
+
 		Department department = deptserv.getDepartmentById(id);
 		DepartmentDto deptDto = new DepartmentDto();
 		deptDto.setDept_id(department.getDept_id());
 		deptDto.setDept_name(department.getDept_name());
-		
+
 		ResponseEntity<Company> company = companyProxy.getCompanyById(department.getCompany_id());
-		
-		Company companyObj = new Company();		
+
+		Company companyObj = new Company();
 		companyObj = company.getBody();
 		deptDto.setCompany(companyObj);
-		
-//		deptDto.setComp_name(company.getBody().getComp_name());		
-//		deptDto.setCompany_id(company.getBody().getCompany_id());
-		
+
 		return ResponseEntity.status(HttpStatus.OK).body(deptDto);
 	}
 	
+	public ResponseEntity<DepartmentDto> handleGetCompanyByIdResponse(Integer id,Exception ex) {
+
+		Department department = deptserv.getDepartmentById(id);
+		DepartmentDto deptDto = new DepartmentDto();
+		deptDto.setDept_id(department.getDept_id());
+		deptDto.setDept_name(department.getDept_name());
+
+		deptDto.setCompany(new Company());
+
+		return ResponseEntity.status(HttpStatus.OK).body(deptDto);
+	}
+
 	@PutMapping("/")
-	public ResponseEntity<ResponseDto> updateDepartment(@RequestBody Department department)
-	{	
+	public ResponseEntity<ResponseDto> updateDepartment(@RequestBody Department department) {
 		int result = deptserv.updateDepartment(department);
-		if(result > 0 ) {
-			return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseDto(HttpStatus.CREATED.toString(), "Department "+department.getDept_name()+" is updated successfully"));
+		if (result > 0) {
+			return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseDto(HttpStatus.CREATED.toString(),
+					"Department " + department.getDept_name() + " is updated successfully"));
+		} else {
+			return ResponseEntity.status(HttpStatus.NOT_MODIFIED)
+					.body(new ResponseDto(HttpStatus.NOT_MODIFIED.toString(),
+							"Department " + department.getDept_name() + " is not updated "));
 		}
-		else {
-			return ResponseEntity.status(HttpStatus.NOT_MODIFIED).body(new ResponseDto(HttpStatus.NOT_MODIFIED.toString(), "Department "+department.getDept_name()+" is not updated "));
-		}		
 	}
 }
